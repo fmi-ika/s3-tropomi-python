1ee3381a2960c2de02b3a890e5251356fa78943a
Tuuli Perttula <perttula@kat-laskenta.fmi.fi>
2022-11-14 12:32:03 +0000

    Added logging and README.md

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # s3-tropomi-python
 Download TROPOMI data from S3 bucket using python boto3.
+
+## Usage
+### Installation
+Needed python packages are listed in `environment.yml` file, which can be used to setup conda environment for running the code.
+
+Setup conda environment: `$ conda env create -f environment.yml -n tropomi`
+
+Activate conda environment: `$ conda activate tropomi`
+
+### Running
+
+Run the code: `$ python download_tropomi.py --var="no2-nrti" --date="20221102"`
+
+Input parameters are:
+- `var`: variable name, which is used to find correct configuration .json file
+- `date`: date to download from S3
+
+### Configurations
+S3 configuration file containing S3 keys should be located in `conf/tropomi_s3.json` and not be under version control. It should contain keys `aws_access_key_id`, `aws_secret_access_key` and `endpoint_url`.
+
+Configurations for each variable are located in `/conf` directory. Configuration .json files are called `variable.json` (e.g. `no2-nrti.json`).
+
+#### Variable S3 configurations
+- `bucket_name`: S3 bucket name for given variable
+- `obj_name_start`: beginning of filename containing placeholders for {date} and {time}
+
+#### Local configurations
+- `path`: local path where the files are downloaded to
+
--- download_tropomi.py
+++ download_tropomi.py
@@ -1,9 +1,11 @@
 import argparse
-import boto3
 import json
 import re
 import datetime
+import logging
+import time
 
+import boto3
 
 def create_s3_client(s3_config_file):
     """ Create S3 client to be used for downloading files.
@@ -15,9 +17,16 @@ def create_s3_client(s3_config_file):
     s3 -- S3 client                                                       
     """
 
-    with open(s3_config_file, "r") as jsonfile:
-        s3_config = json.load(jsonfile)
+    # Read S3 config file
+    logger.debug(f'Reading S3 config file {s3_config_file}')
+    try:
+        with open(s3_config_file, "r") as jsonfile:
+            s3_config = json.load(jsonfile)
+    except Exception as e:
+        logger.error(f'Error while reading the S3 configuration file {s3_config_file}')
+        logger.error(e)
 
+    # Create S3 client
     s3 = boto3.client("s3",
                       aws_access_key_id = s3_config['aws_access_key_id'],
                       aws_secret_access_key = s3_config['aws_secret_access_key'],
@@ -37,14 +46,18 @@ def get_files_containing_pattern(s3, bucket_name, pattern, outpath):
     outpath -- local directory where matching files are downloaded to
 
     """
-    
+
+    # Use paginator to be able to list all objects in bucket containing
+    # more than 1000 objects.
     paginator = s3.get_paginator('list_objects_v2')
     pages = paginator.paginate(Bucket=bucket_name)
 
+    # Download files containing pattern
+    logger.debug(f'Searching for pattern {pattern} in bucket {bucket_name}')
     for page in pages:
         for key in page['Contents']:
             if re.search(pattern, key['Key']):
-                print("Downloading file: ", key['Key'])
+                logger.debug(f'Downloading file {key["Key"]}')
                 local_file = f"{outpath}/{key['Key']}"
                 s3.download_file(bucket_name, key['Key'], local_file)      
             
@@ -52,14 +65,20 @@ def get_files_containing_pattern(s3, bucket_name, pattern, outpath):
 def main():
 
     # Create S3 client
-    s3 = create_s3_client(options.s3_config_file)
+    s3_config_file = "conf/tropomi_s3.json"
+    s3 = create_s3_client(s3_config_file)
 
     # Read variable config
-    variable_config_file = f"conf/{options.variable}.json"
-    with open(variable_config_file, "r") as jsonfile:
-        variable_config = json.load(jsonfile)
+    variable_config_file = f"conf/{options.var}.json"
+    logger.debug(f'Reading config file {variable_config_file}')
+    try:
+        with open(variable_config_file, "r") as jsonfile:
+            variable_config = json.load(jsonfile)
+    except Exception as e:
+        logger.error(f'Error while reading the configuration file {variable_config_file}')
+        logger.error(e)
+    
     bucket_name = variable_config["s3"]["bucket_name"]
-
     time = ""
     pattern = variable_config["s3"]["obj_name_start"].format(date = options.date, time = time)
     
@@ -71,11 +90,7 @@ def main():
 if __name__ == '__main__':                                                      
     #Parse commandline arguments                                                
     parser = argparse.ArgumentParser()
-    parser.add_argument('--s3_config_file',
-                        type = str,
-                        default = 'conf/tropomi_s3.json',                           
-                        help = 'S3 config json file')
-    parser.add_argument('--variable',
+    parser.add_argument('--var',
                         type = str,                                             
                         default = 'no2-nrti',                               
                         help = 'Tropomi variable file to download. Options: no2, so2, co, o3, no2-nrti, so2-nrti, co-nrti, o3-nrti')
@@ -83,5 +98,25 @@ if __name__ == '__main__':
                         type = str,                                             
                         default = '20221103',                               
                         help = 'Date to download from S3.')
-    options = parser.parse_args()                                               
+    parser.add_argument('--loglevel',
+                        default='info',
+                        help='minimum severity of logged messages,\
+                        options: debug, info, warning, error, critical, default=info')
+    
+    options = parser.parse_args()
+
+    # Setup logger                                                               
+    loglevel_dict={'debug':logging.DEBUG,
+                   'info':logging.INFO,
+                   'warning':logging.WARNING,
+                   'error':logging.ERROR,
+                   'critical':logging.CRITICAL}
+    logger = logging.getLogger("logger")
+    logger.setLevel(loglevel_dict[options.loglevel])
+    formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s | (%(filename)s:%(lineno)d)','%Y-%m-%d %H:%M:%S')
+    logging.Formatter.converter = time.gmtime # use utc                                    
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    
     main()
