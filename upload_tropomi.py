23c383e91d1834d41f4281d53a6a2527ebbe7560
Tuuli Perttula <perttula@kat-laskenta.fmi.fi>
2022-11-15 14:07:25 +0000

    Added upload script

--- upload_tropomi.py
+++ upload_tropomi.py
@@ -4,8 +4,11 @@ import re
 import datetime
 import logging
 import time
+import os
 
 import boto3
+from botocore.exceptions import ClientError
+
 
 def create_s3_client(s3_config_file):
     """ Create S3 client to be used for downloading files.
@@ -36,40 +39,37 @@ def create_s3_client(s3_config_file):
     return s3
 
 
-def get_files_containing_pattern(s3, bucket_name, pattern, outpath):
-    """ Download S3 objects containing given pattern.
-                                      
-    Keyword arguments:                                                                                
-    s3 -- boto3 S3 client
-    bucket_name -- S3 bucket name where to search for files
-    pattern -- pattern used for searching matching files
-    outpath -- local directory where matching files are downloaded to
+def upload_file(s3, filename, bucketname, objectname=None):
+    """Upload file to an S3 bucket.
 
+    Keyword arguments:
+    s3 -- S3 client
+    filename -- File to upload
+    bucketname -- Bucket to upload to
+    objectname -- S3 object name. If not specified then filename is used
     """
 
-    # Use paginator to be able to list all objects in bucket containing
-    # more than 1000 objects.
-    paginator = s3.get_paginator('list_objects_v2')
-    pages = paginator.paginate(Bucket=bucket_name)
+    # If S3 object_name was not specified, use file_name
+    if objectname is None:
+        objectname = os.path.basename(filename)
 
-    # Download files containing pattern
-    logger.debug(f'Searching for pattern {pattern} in bucket {bucket_name}')
-    for page in pages:
-        for key in page['Contents']:
-            if re.search(pattern, key['Key']):
-                logger.debug(f'Downloading file {key["Key"]}')
-                local_file = f"{outpath}/{key['Key']}"
-                s3.download_file(bucket_name, key['Key'], local_file)      
-            
+    # Upload the file
+    logger.debug(f'Uploading file {filename} to s3://{bucketname}')
+    try:
+        s3.upload_file(filename, bucketname, objectname)
+    except ClientError as e:
+        logger.error(f'Error while uploading file {filename} to s3://{bucketname}')
+        logger.error(e)
+
 
 def main():
 
     # Create S3 client
-    s3_config_file = "conf/tropomi_s3_ro.json"
+    s3_config_file = "conf/tropomi_s3_rw.json"
     s3 = create_s3_client(s3_config_file)
 
     # Read variable config
-    variable_config_file = f"conf/download/{options.var}.json"
+    variable_config_file = f"conf/upload/{options.var}.json"
     logger.debug(f'Reading config file {variable_config_file}')
     try:
         with open(variable_config_file, "r") as jsonfile:
@@ -79,12 +79,12 @@ def main():
         logger.error(e)
     
     bucket_name = variable_config["s3"]["bucket_name"]
-    time = ""
-    pattern = variable_config["s3"]["obj_name_start"].format(date = options.date, time = time)
-    
-    # Search files including date and download
-    outpath = variable_config["local"]["path"]
-    get_files_containing_pattern(s3, bucket_name, pattern, outpath)
+
+    #Upload data and image files to S3
+    datafile = f'{variable_config["local"]["path"]}/{variable_config["local"]["datafile"].format(date = options.date)}' 
+    imagefile = f'{variable_config["local"]["path"]}/{variable_config["local"]["imagefile"].format(date = options.date)}'
+    upload_file(s3, datafile, bucket_name)
+    upload_file(s3, imagefile, bucket_name)
 
 
 if __name__ == '__main__':                                                      
@@ -92,12 +92,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--var',
                         type = str,                                             
-                        default = 'no2-nrti',                               
-                        help = 'Tropomi variable file to download. Options: no2, so2, co, o3, no2-nrti, so2-nrti, co-nrti, o3-nrti')
+                        default = 'no2-nrti-l3',                               
+                        help = 'Tropomi variable to upload. Options: no2-nrti-l3')
     parser.add_argument('--date',
                         type = str,                                             
-                        default = '20221103',                               
-                        help = 'Date to download from S3.')
+                        default = '20221102',                               
+                        help = 'Date to upload to S3.')
     parser.add_argument('--loglevel',
                         default='info',
                         help='minimum severity of logged messages,\
