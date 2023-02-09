ecb6408abdd9cc1839f8741fafbf324a0532a29c
Tuuli Perttula <perttula@kat-laskenta.fmi.fi>
2023-02-09 13:26:12 +0000

    Added config files for SO2 and O3 processing

--- search_for_dates_in_bucket.py
+++ search_for_dates_in_bucket.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import re
 import datetime
+from dateutil.relativedelta import relativedelta
 import logging
 import time
 import gzip
@@ -72,24 +73,37 @@ def search_for_pattern(s3, bucket_name, pattern):
     return False
 
 
-def daterange(start_date, end_date, reverse = False):
+def daterange(start_date, end_date, product = "l2", reverse = False):
     """ Generator function for getting a list of dates
 
     Keyword arguments:                                                                                
     start_date -- First date in list
     end_date -- Last date in list
+    product -- Options: l2 | l3_day | l3_month
     reverse -- if True, list dates from last to first
 
     Yield:
     list of dates from first to last
 
     """
-    for n in range(int((end_date - start_date).days)):
-        if not reverse:
-            yield start_date + datetime.timedelta(n)
-        else:
-            yield end_date - datetime.timedelta(n)
-            
+    if product == "l2" or product == "l3_day":
+        for n in range(int((end_date - start_date).days)+1):
+            if not reverse:
+                yield (start_date + datetime.timedelta(n)).strftime("%Y%m%d")
+            else:
+                yield (end_date - datetime.timedelta(n)).strftime("%Y%m%d")
+    elif product == "l3_month":
+        delta = relativedelta(end_date, start_date)
+        n_months = delta.months + (delta.years * 12)
+        for n in range(n_months+1):
+            print(n)
+            if not reverse:
+                yield (start_date + relativedelta(months = n)).strftime("%Y%m")
+            else:
+                yield (end_date - relativedelta(months = n)).strftime("%Y%m")
+    else:
+        raise ValueError('Give valid product name.')
+
 
 def main():
 
@@ -107,21 +121,25 @@ def main():
         logger.error(f'Error while reading the configuration file {variable_config_file}')
         logger.error(e)
 
-    start_date = datetime.datetime.strptime(options.start_date,'%Y%m%d').date()
-    end_date = datetime.datetime.strptime(options.end_date,'%Y%m%d').date()
+    # Make datetime objects
+    if options.product == "l3_month":
+        start_date = datetime.datetime.strptime(options.start_date,'%Y%m').date()
+        end_date = datetime.datetime.strptime(options.end_date,'%Y%m').date()
+    else:
+        start_date = datetime.datetime.strptime(options.start_date,'%Y%m%d').date()
+        end_date = datetime.datetime.strptime(options.end_date,'%Y%m%d').date()
 
+    # Loop through dates and check if they are found in bucket
     with open(options.datelist_file, 'w') as outfile_datelist:
-        for single_date in daterange(start_date, end_date, reverse = True):
-            date = single_date.strftime("%Y%m%d")
+        for single_date in daterange(start_date, end_date, product = options.product, reverse = True):
             bucket_name = variable_config[options.product]["bucket_name"]
-            pattern = variable_config[options.product]["obj_name_start"].format(date = date)
+            pattern = variable_config[options.product]["obj_name_start"].format(date = single_date)
     
             # Check if pattern is found in bucket and write found dates in file
             if search_for_pattern(s3, bucket_name, pattern):
-                print(f"{date}")
-                outfile_datelist.write(f"{date}\n")
+                print(f"{single_date}")
+                outfile_datelist.write(f"{single_date}\n")
 
-    
 if __name__ == '__main__':                                                      
     #Parse commandline arguments                                                
     parser = argparse.ArgumentParser()
@@ -131,15 +149,15 @@ if __name__ == '__main__':
                         help = 'Tropomi variable file to download. Options: no2-offl, so2-offl, co-offl, o3-offl, no2-nrti, so2-nrti, co-nrti, o3-nrti')
     parser.add_argument('--product',
                         type = str,
-                        default = 'l2',
+                        default = 'l3_month',
                         help = 'Product to search. Options: l2|l3_day|l3_month')
     parser.add_argument('--start_date',
                         type = str,                          
-                        default = '20220101',                               
+                        default = '202201',                               
                         help = 'First date to search in bucket.')
     parser.add_argument('--end_date',
                         type = str,                                             
-                        default = '20230118',                               
+                        default = '202301',                               
                         help = 'Last date to search in bucket.')
     parser.add_argument('--datelist_file',
                         type = str,                                             
